0f2bbc76c59c89e15a296ed8b3acd4fad8b66809
Ben Stahl <bhstahl@gmail.com>
2015-11-25 20:28:46 -0500

    Add coverage for server and stores

--- test/DataStore.js
+++ test/DataStore.js
@@ -6,18 +6,38 @@ const File = require('../lib/models/File');
 
 describe('DataStore', () => {
     const datastore = new DataStore({path: '/files'});
+
+    it('constructor must require a path', (done) => {
+        assert.throws(() => { new DataStore() }, Error);
+        done();
+    });
+
     it('should provide extensions', (done) => {
         datastore.should.have.property('extensions');
         assert.equal(datastore.extensions, null);
 
-        datastore.extensions = [ 'creation', 'expiration']
+        datastore.extensions = [ 'creation', 'expiration'];
         assert.equal(datastore.extensions, 'creation,expiration');
+        done();
+    });
+
+    it('extensions must be an array', (done) => {
+        assert.throws(() => {
+            datastore.extensions = 'creation, expiration';
+        }, Error);
         done();
     });
 
     it('must have a create method', (done) => {
         datastore.should.have.property('create');
         datastore.create(new File(1));
+        done();
+    });
+
+    it('create() must require a File', (done) => {
+        assert.throws(() => {
+            datastore.create();
+        }, Error);
         done();
     });
 
--- test/Server.js
+++ test/Server.js
@@ -8,7 +8,18 @@ const Server = require('../lib/Server');
 const DataStore = require('../lib/stores/DataStore');
 const TUS_RESUMABLE = require('../lib/constants').TUS_RESUMABLE;
 
+let hasHeader = (res, header) => {
+    let key = Object.keys(header)[0];
+    return res._header.indexOf(`${key}: ${header[key]}`) > -1;
+}
+
 describe('Server', () => {
+
+    it('datastore setter must require a DataStore subclass', (done) => {
+        assert.throws(() => { server.datastore = {}; }, Error);
+        done();
+    });
+
     it('setting the DataStore should attach handlers', (done) => {
         const server = new Server();
         server.handlers.should.be.empty();
@@ -76,10 +87,29 @@ describe('Server', () => {
           .expect(404, 'Not found', done)
     });
 
-    it('#listen should create an instance of http.Server', (done) => {
+    it('listen() should create an instance of http.Server', (done) => {
       let new_server = server.listen();
       assert.equal(new_server instanceof http.Server, true)
       done();
     });
 
+    it('handle() should allow overriding the HTTP method', (done) => {
+      let req = { headers: { 'x-http-method-override': 'OPTIONS' }, method: 'GET'};
+      let res = new http.ServerResponse({ method: 'OPTIONS'});
+      server.handle(req, res);
+      assert.equal(req.method, 'OPTIONS')
+      done();
+    });
+
+    it('handle() should allow overriding the HTTP method', (done) => {
+      let origin = 'vimeo.com';
+      let req = { headers: { origin }, method: 'OPTIONS', url: '/'};
+      let res = new http.ServerResponse({ method: 'OPTIONS'});
+      server.handle(req, res);
+      console.log(res._header)
+      assert.equal(hasHeader(res, {
+        'Access-Control-Allow-Origin': origin,
+       }), true)
+      done();
+    });
 });
