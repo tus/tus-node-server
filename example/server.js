08546df2275adfc976783834cb7fff3020080d85
bhstahl <bhstahl@gmail.com>
2016-05-05 14:57:05 -0400

    Init GCSDataStore

--- example/server.js
+++ example/server.js
@@ -3,8 +3,12 @@
 const tus = require('../index');
 
 const server = new tus.Server();
-server.datastore = new tus.FileStore({
+
+server.datastore = new tus.GCSDataStore({
     path: '/files',
+    projectId: 'project-id',
+    keyFilename: 'path/to/your/keyfile.json',
+    bucket: 'bucket-name',
 });
 
 const host = '127.0.0.1';
