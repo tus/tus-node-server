509d5ede26ae4ce92762fdae55888db4ffb009f7
bhstahl <bhstahl@gmail.com>
2016-05-06 11:31:56 -0400

    PATCH should 409 if the Upload-Offset header is a mismatch

--- test/Test-PatchHandler.js
+++ test/Test-PatchHandler.js
@@ -1,25 +1,70 @@
+/* eslint-env node, mocha */
 'use strict';
 
 const assert = require('assert');
-const should = require('should');
 const http = require('http');
 const PatchHandler = require('../lib/handlers/PatchHandler');
 const DataStore = require('../lib/stores/DataStore');
 
-
-let pluckBody = (res) => {
-    return /\n(.*)$/.exec(res.output[0])[1];
-}
+const hasHeader = (res, header) => {
+    const key = Object.keys(header)[0];
+    return res._header.indexOf(`${key}: ${header[key]}`) > -1;
+};
 
 describe('PatchHandler', () => {
+    const path = '/files';
     let res = null;
-    let store = new DataStore({ path: '/files' });
-    let handler = new PatchHandler(store);
-    let req = { headers: {} };
+    const store = new DataStore({ path });
+    const handler = new PatchHandler(store);
+    const req = { headers: {} };
 
     beforeEach((done) => {
-        const METHOD = 'PATCH';
-        res = new http.ServerResponse({ method: METHOD });
+        res = new http.ServerResponse({ method: 'PATCH' });
         done();
     });
+
+    it('should 403 if no Content-Type header is not application/offset+octet-stream', (done) => {
+        req.headers = {};
+        req.url = `${path}/1234`;
+        handler.send(req, res);
+        assert.equal(res.statusCode, 403);
+        done();
+    });
+
+    it('should 403 if no Upload-Offset header', (done) => {
+        req.headers = { 'content-type': 'application/offset+octet-stream' };
+        req.url = `${path}/1234`;
+        handler.send(req, res);
+        assert.equal(res.statusCode, 403);
+        done();
+    });
+
+    describe('send()', () => {
+        it('must return a promise if the headers validate', () => {
+            req.headers = {
+                'upload-offset': 0,
+                'content-type': 'application/offset+octet-stream',
+            };
+            req.url = `${path}/1234`;
+            return handler.send(req, res).should.be.a.Promise();
+        });
+
+        it('must 409 if the offset does not match', (done) => {
+            req.headers = {
+                'upload-offset': 10,
+                'content-type': 'application/offset+octet-stream',
+            };
+            req.url = `${path}/1234`;
+
+
+            return handler.send(req, res)
+                .then(() => {
+                    assert.equal(res.statusCode, 409);
+                    return;
+                })
+                .then(done)
+                .catch(done);
+        });
+    });
+
 });
