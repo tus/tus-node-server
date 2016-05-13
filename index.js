ac5d2c0b78db1cb61225c468ea5758ce4c922388
Ben Stahl <bens@vimeo.com>
2016-05-13 14:55:28 -0400

    Create GCSDataStore

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 const Server = require('./lib/Server');
 const DataStore = require('./lib/stores/DataStore');
 const FileStore = require('./lib/stores/FileStore');
+const GCSDataStore = require('./lib/stores/GCSDataStore');
 
 module.exports = {
     Server,
     DataStore,
     FileStore,
+    GCSDataStore,
 };
