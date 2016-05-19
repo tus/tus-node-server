037ae51850e934dd0bec2e5d88a1f4fe524d5d0f
Ben Stahl <bens@vimeo.com>
2016-05-19 14:23:09 -0400

    Don't export GCSDataStore till it is ready

--- index.js
+++ index.js
@@ -3,11 +3,9 @@
 const Server = require('./lib/Server');
 const DataStore = require('./lib/stores/DataStore');
 const FileStore = require('./lib/stores/FileStore');
-const GCSDataStore = require('./lib/stores/GCSDataStore');
 
 module.exports = {
     Server,
     DataStore,
     FileStore,
-    GCSDataStore,
 };
