6f6d4faf4725d2db9735d329e08f75b76108f7ff
Bradley Arsenault <bradley.allen.arsenault@gmail.com>
2016-11-11 16:21:55 -0500

    Changed index.js to include MongoGridFSStore

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Server = require('./lib/Server');
 const DataStore = require('./lib/stores/DataStore');
 const FileStore = require('./lib/stores/FileStore');
 const GCSDataStore = require('./lib/stores/GCSDataStore');
+const MongoGridFSStore = require('./lib/stores/MongoGridFSStore');
 const EVENTS = require('./lib/constants').EVENTS;
 
 module.exports = {
@@ -11,5 +12,6 @@ module.exports = {
     DataStore,
     FileStore,
     GCSDataStore,
+    MongoGridFSStore
     EVENTS,
 };
