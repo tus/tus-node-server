ccc6e69a2d01488f66682bb7e3d44032c065bdb9
Acconut <maerious@gmail.com>
2018-07-21 22:13:23 +0200

    Capitalize filename for S3Store

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Server = require('./lib/Server');
 const DataStore = require('./lib/stores/DataStore');
 const FileStore = require('./lib/stores/FileStore');
 const GCSDataStore = require('./lib/stores/GCSDataStore');
-const S3Store = require('./lib/stores/s3Store');
+const S3Store = require('./lib/stores/S3Store');
 const ERRORS = require('./lib/constants').ERRORS;
 const EVENTS = require('./lib/constants').EVENTS;
 
