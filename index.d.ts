06cfb28efa739569b8e0992c6021d8b09df50a3a
KyoungDuck <skate2@naver.com>
2020-01-17 13:04:45 +0900

    update typescript(earse private method, change constant)

--- index.d.ts
+++ index.d.ts
@@ -53,7 +53,6 @@ export declare class DataStore extends EventEmitter{
  */
 export declare class FileStore extends DataStore{
     constructor(options : FileStoreOptions);
-    _checkOrCreateDirectory(): any;
     create(req: http.IncomingMessage): Promise<any>;
     write(req: http.IncomingMessage, file_id?: string, offset?: number): Promise<any>;
     getOffset(file_id: string): Promise<any>;
@@ -64,7 +63,6 @@ export declare class FileStore extends DataStore{
  */
 export declare class GCSDataStore extends DataStore{
     constructor(options: GCStoreOptions);
-    _getBucket(): any;
     create(req: http.IncomingMessage): Promise<any>;
     write(req: http.IncomingMessage, file_id?: string, offset?: number): Promise<any>;
     getOffset(file_id: string): Promise<any>;
@@ -75,17 +73,6 @@ export declare class GCSDataStore extends DataStore{
  */
 export declare class S3Store extends DataStore {
     constructor(options : S3StoreOptions);
-    _bucketExists(): Promise<any>;
-    _initMultipartUpload(file: File): Promise<any>;
-    _saveMetadata(file: File, upload_id: string): Promise<any>;
-    _getMetadata(file_id: string): Promise<any>;
-    _parseMetadataString(metadata_string: string): any;
-    _uploadPart(metadata: any, read_stream: any, current_part_number: number): Promise<string>;
-    _processUpload(metadata: any, req: http.IncomingMessage, current_part_number: number): Promise<number>;
-    _finishMultipartUpload(metadata: any, parts: Array<any>): Promise<string>;
-    _retrieveParts(file_id: string, part_number_marker: string): Promise<Array<any>>;
-    _countParts(file_id: string): Promise<number>;
-    _clearCache(file_id: string): any;
     create(req: http.IncomingMessage): Promise<any>;
     write(req: http.IncomingMessage, file_id?: string, offset?: number): Promise<any>;
     getOffset(file_id: string, with_parts?: boolean): Promise<any>;    
@@ -103,47 +90,43 @@ export declare class Server extends EventEmitter{
     listen(): http.Server;
 }
 
-export declare const ERRORS: {
-    MISSING_OFFSET: {
-        status_code: 403,
-        body: 'Upload-Offset header required\n',
-    },
-    INVALID_CONTENT_TYPE: {
-        status_code: 403,
-        body: 'Content-Type header required\n',
-    },
-    FILE_NOT_FOUND: {
-        status_code: 404,
-        body: 'The file for this url was not found\n',
-    },
-    INVALID_OFFSET: {
-        status_code: 409,
-        body: 'Upload-Offset conflict\n',
-    },
-    FILE_NO_LONGER_EXISTS: {
-        status_code: 410,
-        body: 'The file for this url no longer exists\n',
-    },
-    INVALID_LENGTH: {
-        status_code: 412,
-        body: 'Upload-Length or Upload-Defer-Length header required\n',
-    },
-    UNKNOWN_ERROR: {
-        status_code: 500,
-        body: 'Something went wrong with that request\n',
-    },
-    FILE_WRITE_ERROR: {
-        status_code: 500,
-        body: 'Something went wrong receiving the file\n',
-    },
+export declare const EVENTS: {
+    EVENT_ENDPOINT_CREATED: string;
+    EVENT_FILE_CREATED: string;
+    EVENT_UPLOAD_COMPLETE: string;
 };
 
-declare const EVENT_ENDPOINT_CREATED = 'EVENT_ENDPOINT_CREATED';
-declare const EVENT_FILE_CREATED = 'EVENT_FILE_CREATED';
-declare const EVENT_UPLOAD_COMPLETE = 'EVENT_UPLOAD_COMPLETE';
-
-export declare const EVENTS :{
-    EVENT_ENDPOINT_CREATED: string,
-    EVENT_FILE_CREATED: string,
-    EVENT_UPLOAD_COMPLETE: string,
+export declare const ERRORS: {
+    MISSING_OFFSET: {
+        status_code: number;
+        body: string;
+    };
+    INVALID_CONTENT_TYPE: {
+        status_code: number;
+        body: string;
+    };
+    FILE_NOT_FOUND: {
+        status_code: number;
+        body: string;
+    };
+    INVALID_OFFSET: {
+        status_code: number;
+        body: string;
+    };
+    FILE_NO_LONGER_EXISTS: {
+        status_code: number;
+        body: string;
+    };
+    INVALID_LENGTH: {
+        status_code: number;
+        body: string;
+    };
+    UNKNOWN_ERROR: {
+        status_code: number;
+        body: string;
+    };
+    FILE_WRITE_ERROR: {
+        status_code: number;
+        body: string;
+    };
 };
