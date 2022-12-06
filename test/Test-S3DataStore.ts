eaafa5e219d3058c103ea3aa2944e02c0aa8bb65
Merlijn Vos <merlijn@soverin.net>
2022-12-06 11:54:18 -0300

    S3Store: handle chunks under the minimum S3 part size (#341)
    - Handle chunks under the minimum S3 part size. It does so by uploading chunks under 5MB as a regular upload to S3 with key as the upload ID + `.part`. On the next call to `store.write()`, we check if there is a incomplete part and prepend the buffer from the returned request into chunk we were about to upload. We don't need a temporary file for this incomplete part on disk so we only need to send a delete request when we're done with it.
    - Small optimizations to reduce network requests.
    - New tests

--- test/Test-S3DataStore.ts
+++ test/Test-S3DataStore.ts
@@ -1,9 +1,15 @@
 import path from 'node:path'
+import fs from 'node:fs/promises'
+import assert from 'node:assert/strict'
+import {Readable} from 'node:stream'
+
+import sinon from 'sinon'
 
 import S3Store from '../lib/stores/S3Store'
 import * as shared from './Test-Stores.shared'
+import Upload from '../lib/models/Upload'
 
-describe('S3DataStore', () => {
+describe('S3DataStore', function () {
   before(function () {
     this.testFileSize = 960_244
     this.testFileName = 'test.mp4'
@@ -19,6 +25,47 @@ describe('S3DataStore', () => {
       partSize: 8 * 1024 * 1024, // Each uploaded part will have ~8MB,
     })
   })
+
+  it('should correctly prepend a buffer to a file', async function () {
+    const path = 'test/fixtures/foo.txt'
+    await fs.writeFile(path, 'world!')
+    await this.datastore.prependIncompletePart(path, Buffer.from('Hello, '))
+    assert(await fs.readFile(path, 'utf8'), 'Hello, world!')
+    await fs.unlink(path)
+  })
+
+  it('should store in between chunks under the minimum part size and prepend it to the next call', async function () {
+    const store = this.datastore
+    const size = 1024
+    const incompleteSize = 1024
+    const getIncompletePart = sinon.spy(store, 'getIncompletePart')
+    const deleteIncompletePart = sinon.spy(store, 'deleteIncompletePart')
+    const uploadIncompletePart = sinon.spy(store, 'uploadIncompletePart')
+    const uploadPart = sinon.spy(store, 'uploadPart')
+    const upload = new Upload({
+      id: 'incomplete-part-test',
+      size: size + incompleteSize,
+      offset: 0,
+    })
+
+    await store.create(upload)
+    const n1 = await store.write(
+      Readable.from(Buffer.alloc(incompleteSize)),
+      upload.id,
+      upload.offset
+    )
+    assert.equal(n1, incompleteSize)
+    const n2 = await store.write(Readable.from(Buffer.alloc(size)), upload.id, n1)
+    assert.equal(n2, incompleteSize + size)
+    const {offset} = await store.getUpload(upload.id)
+
+    assert.equal(getIncompletePart.calledTwice, true)
+    assert.equal(deleteIncompletePart.calledOnce, true)
+    assert.equal(uploadIncompletePart.calledOnce, true)
+    assert.equal(uploadPart.calledOnce, true)
+    assert.equal(offset, size + incompleteSize)
+  })
+
   shared.shouldHaveStoreMethods()
   shared.shouldCreateUploads()
   // Termination extension not implemented yet
